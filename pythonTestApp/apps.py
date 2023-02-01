ff215ba3a3c39831cfcc3c4e20ffef744f5dfe0b
luoshiling <luoshiling@kuaishou.com>
2023-02-01 19:23:04 +0800

    init

---
+++ pythonTestApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PythontestappConfig(AppConfig):
+    name = 'pythonTestApp'
---
+++ pythonTestApp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(http_method_names=['POST'])
+def createOrder(request):
+    data = json.loads(request.body)
+    if "orderId" not in data:
+        return Response({
+            "status_code": 400,
+            'msg': "orderId"
+        })
+    elif "amount" not in data:
+        return Response({
+            "status_code": 400,
+            'msg': "amount"
+        })
+    elif data["orderId"] == "" or data["orderId"] == " ":
+        print(data)
+        return Response({
+            "status_code": 400,
+            'msg': "orderId"
+        })
+    elif data["amount"] == "" or data["amount"] == " ":
+        return Response({
+            "status_code": 400,
+            'msg': "amount"
+        })
+    else:
+        return Response({
+            "status_code": 200,
+            'msg': "订单："+str(data["orderId"])+ "，创建成功"
+        })
+
+
