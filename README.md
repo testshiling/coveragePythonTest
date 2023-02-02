1dcb0a9cae5d6b7cb132befc98c1f476ba6acca6
luoshiling <luoshiling@kuaishou.com>
2023-02-02 14:33:03 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pythonCoverageTest
+# coveragePythonTest
+## 使用方式
+1.在项目根目录下（manage.py所在目录）
+输入 coverage  erase  清楚原来的覆盖率数据
+
+2.启动项目，在项目根目录下
+输入 python manage.py runserver 0.0.0.0:8000
+
+3.调用接口
+url=127.0.0.1:8000/create/
+body={
+    "orderId":1,
+    "amount":2
+}
+
+4.在项目根目录下， CONTROL+C关闭项目进程，收集覆盖率
+
+5.产出覆盖率报告
+输入 coverage combine（合并报告）coverage report（查看报告）coverage html（产出html报告）
--- coveragePythonTest/wsgi.py
+++ coveragePythonTest/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+import coverage,atexit
+cov = coverage.Coverage(branch=True,config_file='.coveragerc')
+cov.start()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +18,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coveragePythonTest.settings")
 
 application = get_wsgi_application()
+
+def save_coverage():
+    cov.stop()
+    cov.save()
+
+atexit.register(save_coverage)
